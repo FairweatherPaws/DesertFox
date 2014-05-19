81f5d164593a41b1339ca6eeb2a802ed95a54727
Alexander Solan <alexander.solan@gmail.com>
2014-05-19 15:34:59 +0300

    Initial commit.

---
+++ Assets/Scripts/CloudRoll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudRoll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		renderer.material.mainTextureOffset += new Vector2(Time.deltaTime/100, 0);
+
+	}
+}
